f6e6f98a4ff8ef03163d9a8f741477ae17664a0a
Nandini <cse1732.iiitbhopal@gmail.com>
2020-05-17 07:50:14 +0530

    First commit

---
+++ sites/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
